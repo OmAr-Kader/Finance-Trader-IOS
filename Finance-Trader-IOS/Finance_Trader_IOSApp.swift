1023bb10e8cfb8bb7d4acd1ff33530199fc12e24
OmAr-Kader <ramooor700@gmail.com>
2024-04-24 00:39:50 +0200

    Initial Commit

---
+++ Finance-Trader-IOS/Finance_Trader_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_Trader_IOSApp.swift
+//  Finance-Trader-IOS
+//
+//  Created by OmAr on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Finance_Trader_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
